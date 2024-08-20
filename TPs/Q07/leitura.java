968a497c3189a39ab73585df26f707f506d5c9c3
gabsnim <gabrielxb110@gmail.com>
2024-08-19 22:35:54 -0300

    TP sla..

---
+++ TPs/Q07/leitura.java
@@ -0,0 +1,22 @@
+class leitura
+{
+
+
+
+
+
+
+
+    
+
+    public static void main(String[] args)
+    {
+        String nome;
+        String endereco;
+        nome = MyIO.readLine();
+        endereco = MyIO.readLine();
+        
+    }
+
+
+}
