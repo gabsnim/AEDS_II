7e2df9f17fcb7d81562683956f0ec60b26f36849
gabsnim <gabrielxb110@gmail.com>
2024-09-05 11:45:59 -0300

    verdecaiu nice

--- lab/04/sort.java
+++ lab/04/sort.java
@@ -1,14 +1,45 @@
 class sort
 {
 
+    public static boolean isPar (int x)
+    {
+        boolean r = true;
+        if(x % 2 != 0)
+        {
+            r = false;
+        }
+        return r;
+    }
+
+    public static int[] swap (int[] array, int i, int j)
+    {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+        return array;
+    }
 
     public static int[] sort (int n, int m, int array[])
     {
 
-        for(int i = 1; i < n; i++)
+        for(int i = 0; i < n - 1; i++)
         {
-            
+            int menor = i;
+
+            for(int j = (i + 1); j < n; j++)
+            {
+                if(array[menor] % m > array[j] % m)
+                {
+                    if(array[menor] % m == array[j] % m)
+                    {
+                        
+                    }
+                    menor = j;
+                }
+            }
+            array = swap(array, menor, i);
         }
+        return array;
     }
 
     public static void main (String[] args)
@@ -26,6 +57,16 @@ class sort
             array[i] = MyIO.readInt();
         }
 
-        sort(n,m,array);
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(" "+   array[i]);
+        }
+        System.out.println("");
+        array = sort(n,m,array);
+
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(" "+   array[i]);
+        }
     }
-}
+}   
